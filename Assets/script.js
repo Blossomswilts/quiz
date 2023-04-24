8d34fca0633fb3ae0c99028c24331eb01511fe43
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-04-24 13:26:38 -0400

    HTML CSS

---
+++ Assets/script.js
@@ -0,0 +1,36 @@
+//________Query Selectors pull data from HTML___________________
+
+
+//_________Quiz Array___________________________________________
+
+
+//_________Trigger Start Button_________________________________
+
+
+//_________Trigger Next Button__________________________________
+
+
+//_________Trigger Restart Button_______________________________
+
+
+//_________Timer Countdown from 60s_____________________________
+
+
+//_________Display Quiz Contaier________________________________
+
+
+//_________Create Quiz__________________________________________
+
+
+//_________Check True or False__________________________________
+
+
+//_________Create initial function for start button_____________
+
+
+//_________Confirm display on high scores with local Storage____
+
+
+
+
+
